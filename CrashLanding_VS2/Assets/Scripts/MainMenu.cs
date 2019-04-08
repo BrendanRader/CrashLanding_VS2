389f616c397967b351e3a33fe1e858b65677d6e7
Akorinza <42954528+Akorinza@users.noreply.github.com>
2019-04-08 03:24:21 -0400

    Eric's second update
    In this update we've implemented a number of new menus.

--- CrashLanding_VS2/Assets/Scripts/MainMenu.cs
+++ CrashLanding_VS2/Assets/Scripts/MainMenu.cs
@@ -8,11 +8,30 @@ public class MainMenu : MonoBehaviour
    public void PlayGame()
     {
         SceneManager.LoadScene("Level1");
+        Debug.Log("Loading Level1...");
     }
 
     public void QuitGame()
     {
         Debug.Log("The game is quitting...");
         Application.Quit();
+    }
+
+    public void HowtoplayGame()
+    {
+        SceneManager.LoadScene("HowtoplayAlien");
+        Debug.Log("Going to game instructions...");
+    }
+
+    public void PlayerObjectives()
+    {
+        SceneManager.LoadScene("PlayerObjectives");
+        Debug.Log("Going to player objectives...");
+    }
+
+    public void WinLoseConditions()
+    {
+        SceneManager.LoadScene("WinLoseConditions");
+        Debug.Log("Going to the Win/Lose conditions...");
     }
 }

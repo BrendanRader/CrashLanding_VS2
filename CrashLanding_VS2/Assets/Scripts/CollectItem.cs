be766c6b824f9adad3e978806c5e214db9c3436e
Emily Liberaski <emliberaski@yahoo.com>
2019-04-08 00:20:05 -0400

    ElevatorDoorAdded

---
+++ CrashLanding_VS2/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour {
+
+    public Item item;
+    public static int collected = 0;
+
+    Animator animator;
+    bool doorOpen;
+
+    void Awake() {
+        collected++;
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider plyr) {
+
+        if (plyr.gameObject.tag == "Player"){
+            collected--;
+            Inventory.instance.Add(item);
+            Destroy(gameObject);
+
+            if(collected == 0){
+              doorOpen = true;
+              Doors ("Open");
+            }
+        }
+    }
+
+    void Doors(string direction){
+      animator.SetTrigger(direction);
+      }
+    }

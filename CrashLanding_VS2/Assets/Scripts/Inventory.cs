3d98c5d6df34b900963477b2edcd253b7fc8cd51
Akorinza <42954528+Akorinza@users.noreply.github.com>
2019-04-08 05:39:49 -0400

    Eric's third update
    This update has most of the essential items added and everything just needs a little bit of tweaking at this point.

---
+++ CrashLanding_VS2/Assets/Scripts/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inventory : MonoBehaviour
+{
+    public static bool InventoryIsActive = false;
+
+    public GameObject InventoryUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (InventoryIsActive)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        InventoryUI.SetActive(false);
+        Time.timeScale = 1f;
+        InventoryIsActive = false;
+    }
+
+    void Pause()
+    {
+        InventoryUI.SetActive(true);
+        Time.timeScale = 0f;
+        InventoryIsActive = true;
+    }
+}

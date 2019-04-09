55554036da89b71bf2f0539e59bca4f4f1781261
BrendanRader <37194152+BrendanRader@users.noreply.github.com>
2019-04-09 07:59:23 -0400

    Revert "Rose Walks"
    This reverts commit b4ee5738e805862fe5e28a57912baf5582399285.

---
+++ CrashLanding_VS2/Assets/Scripts/CL_Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CL_Health : MonoBehaviour
+{
+    //starting health
+    [SerializeField]
+    private int startHealth = 5;
+
+    //keep track of current health
+    public int currentHealth;
+
+    //reset health when enabled
+    private void OnEnable()
+    {
+        currentHealth = startHealth;
+    }
+
+    //Method to take damage
+    public void TakeDamage(int damageAmount)
+    {
+        //Subtract damage taken from current health
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false); // disables game object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

876ccd26728905d143ad65f63c0549835c0468d0
Emily Liberaski <emliberaski@yahoo.com>
2019-04-08 20:40:03 -0400

    WinScreen

---
+++ CrashLanding_VS2/Assets/Scripts/WinScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScreen : MonoBehaviour
+{
+
+  public GameObject WinCanvas;
+
+    // Start is called before the first frame update
+    void Awake() {
+        WinCanvas.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider plyr){
+      if (plyr.gameObject.tag == "Player"){
+        WinCanvas.SetActive(true);
+        Time.timeScale = 0f;
+    }
+  }
+}

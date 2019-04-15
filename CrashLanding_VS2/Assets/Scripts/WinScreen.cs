0c906e19840a3fbf71970043bb76bdb09476f511
Akorinza <42954528+Akorinza@users.noreply.github.com>
2019-04-14 22:42:56 -0400

    Adding the scoreboard

--- CrashLanding_VS2/Assets/Scripts/WinScreen.cs
+++ CrashLanding_VS2/Assets/Scripts/WinScreen.cs
@@ -7,6 +7,7 @@ public class WinScreen : MonoBehaviour
 {
 
   public GameObject WinCanvas;
+  public GameObject ScoreboardCanvas;
 
     // Start is called before the first frame update
     void Awake() {
@@ -16,7 +17,8 @@ public class WinScreen : MonoBehaviour
     void OnTriggerEnter(Collider plyr){
       if (plyr.gameObject.tag == "Player"){
         WinCanvas.SetActive(true);
+        ScoreboardCanvas.SetActive(true);
         Time.timeScale = 0f;
-    }
+        }
   }
 }

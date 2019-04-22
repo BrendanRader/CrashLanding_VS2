967836565f4c3a632fd742b1a1f6b08235c3e715
Emily Liberaski <emliberaski@yahoo.com>
2019-04-22 17:19:11 -0400

    Final Level Win Collider

---
+++ CrashLanding_VS2/Assets/Scripts/FinalLevelCollectItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalLevelCollectItem : MonoBehaviour {
+
+    public Item item;
+    public static int collected = 0;
+    public GameObject WinCollider;
+
+    void Awake() {
+        collected++;
+        WinCollider.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider plyr) {
+
+        if (plyr.gameObject.tag == "Player"){
+            collected--;
+            Inventory.instance.Add(item);
+            Destroy(gameObject);
+
+            if(collected == 0){
+              WinCollider.SetActive(true);
+            }
+        }
+    }
+  }

8924b17fbdb8a3fa33b7ab88ea17c0dd272ed94f
Akorinza <42954528+Akorinza@users.noreply.github.com>
2019-04-22 16:46:39 -0400

    Updating the cameras to switch numbers

---
+++ CrashLanding_VS2/Assets/Scripts/CameraButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraButton : MonoBehaviour
+{
+    public GameObject Camera1;
+
+    void Update()
+    {
+        if (Input.GetKey("x"))
+            Camera1.SetActive(true);
+    }
+}

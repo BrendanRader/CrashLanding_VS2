3dea663b16b14d84c9b61d300afc326d68b7134e
Akorinza <42954528+Akorinza@users.noreply.github.com>
2019-04-22 15:42:01 -0400

    Updating the volume slider

---
+++ CrashLanding_VS2/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+
+    public Slider slider;
+    public AudioSource audioSource;
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSource.volume = slider.value;
+    }
+}

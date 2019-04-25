564d6608574d4938f3daf4f7763a474787b24896
BrendanRader <37194152+BrendanRader@users.noreply.github.com>
2019-04-24 22:24:30 -0400

    Enemy Attack WIP

---
+++ CrashLanding_VS2/Assets/Scripts/CL_EnemyController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CL_EnemyController : MonoBehaviour
+{
+    private Animator animator;
+
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    [SerializeField]
+    [Range(1, 10)]
+    private int damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Animator>();
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                attack();
+                FaceTarget();
+            }
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    void attack()
+    {
+        //trigger slap animation here
+
+        var health = target.GetComponent<CL_PlayerHealth>();
+        if (health != null)
+            health.TakeDamage(damage);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}

bc3bd3ab1b25c7cd4870e371f10583b90f6c7d37
Emily Liberaski <emliberaski@yahoo.com>
2019-04-25 17:17:26 -0400

    updated scripts

--- CrashLanding_VS2/Assets/Scripts/CL_EnemyController.cs
+++ CrashLanding_VS2/Assets/Scripts/CL_EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class CL_EnemyController : MonoBehaviour
 {
-    private Animator animator;
+    public Animator animator;
 
     [SerializeField]
     private int damage = 10;
@@ -15,12 +15,12 @@ public class CL_EnemyController : MonoBehaviour
     Transform target;
     NavMeshAgent agent;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //gameObject.GetComponent<Animator>();
+        animator = GetComponentInChildren<Animator>();
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
     }
@@ -52,7 +52,7 @@ public class CL_EnemyController : MonoBehaviour
 
     void attack()
     {
-        //Call Animation Here!!!!!
+        animator.SetTrigger ("slap");
 
         target.GetComponent<CL_PlayerHealth>().TakeDamage(damage);
     }
--- CrashLanding_VS2/Assets/Scripts/EnemyAttack.cs
+++ CrashLanding_VS2/Assets/Scripts/EnemyAttack.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour {
 
+public Animator animator;
+
+void Awake()
+{
+  animator = GetComponentInChildren<Animator>();
+}
 
 void OnTriggerEnter(Collider plyr) {
 
     if (plyr.gameObject.tag == "Player"){
     GameObject.Find("Player").GetComponent<CL_PlayerHealth>().currentHealth = GameObject.Find("Player").GetComponent<CL_PlayerHealth>().currentHealth - 5;
+    animator.SetTrigger ("slap");
     }
   }
 }
